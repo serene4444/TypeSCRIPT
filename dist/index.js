02161f8819dde4bdd7e6c3479418b79b233dfea7
Plummer, Serene <sereneplummer@my.unt.edu>
2025-11-26 13:51:04 -0600

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log('Hello from TypeScript');
+console.log(greet('world'));
+//# sourceMappingURL=index.js.map
---
+++ extensions/README.md
@@ -0,0 +1,11 @@
+# TypeScript Project
+
+Small TypeScript project scaffolded by the assistant.
+
+Quick start
+
+```powershell
+npm install
+npm run build
+npm start
+```
