2e3b87f020b3f9088181fa666eba64b7ede295a9
Plummer, Serene <sereneplummer@my.unt.edu>
2026-01-17 19:17:01 -0600

    Add city.js and city.ts files for city data handling

---
+++ city.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var city_js_1 = require("./city.js");
+var city = {
+    name: "Toledo",
+    country: "Spain"
+};
+console.log((0, city_js_1.formatCity)(city));
